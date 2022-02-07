b90ffad4915d19aace48c763f1f563ac6532b804
fanse2 <fanse2@gmail.com>
2022-02-07 01:33:35 -0800

    add router

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/users");
 
 app.use(express.static("public"));
 //npm i ejs
 app.set("view engine", "ejs");
+
+app.use("/users", userRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { name: "June" });
